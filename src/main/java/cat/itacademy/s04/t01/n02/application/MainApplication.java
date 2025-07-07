b9fec71a97ffa7ed6328d58eb9c0519e4c98a0a4
Pablo Gómez San Joaquín <pablo.federico.gomez@gmail.com>
2025-07-07 11:22:13 +0200

    Fixbug: Correciones de Romina

---
+++ src/main/java/cat/itacademy/s04/t01/n02/application/MainApplication.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t01.n02.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
--- src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
+++ src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
@@ -1,32 +1,23 @@
 package cat.itacademy.s04.t01.n02.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-
 public class HelloWorldController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldController.class, args);
-	}
-
-	//http://localhost:9001/HelloWorld
-	//http://localhost:9001/HelloWorld?name=Pablo
-
-	@GetMapping("/HelloWorld")
+	// Ruta: http://localhost:9001/HelloWorld2
+	// Ruta: http://localhost:9001/HelloWorld2?name=Pablo
+	@GetMapping("/HelloWorld2")
 	public String hello(@RequestParam(value = "name", defaultValue = "Unknown") String name) {
 		return String.format("Hello %s! \n" +
 				"You are running a Gradle project", name);
 	}
-	//http://localhost:9001/HelloWorld2
-	//http://localhost:9001/HelloWorld2/Pablo
 
+	// Ruta: http://localhost:9001/HelloWorld2
+	// Ruta: http://localhost:9001/HelloWorld2/Pablo
 	@GetMapping("/HelloWorld2/{name}")
 	public String hello2(@PathVariable String name) {
 		return String.format("Hello %s! \n" +
